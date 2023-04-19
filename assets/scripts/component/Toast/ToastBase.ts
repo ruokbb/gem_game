f66fdafab80c15366e59cea7325841308863dac6
shiqixin.set <shiqixin.set@bytedance.com>
2023-04-19 11:18:52 +0800

    [update] Toast 动画

--- assets/scripts/component/Toast/ToastBase.ts
+++ assets/scripts/component/Toast/ToastBase.ts
@@ -1,4 +1,4 @@
-import {Node,tween,UITransform, BlockInputEvents, Component, _decorator, UIOpacity, Vec3} from 'cc'
+import {Node,tween,UITransform, Vec2,Widget, BlockInputEvents, Component, _decorator, UIOpacity, Vec3} from 'cc'
 const { ccclass, property } = _decorator;
 
 /**
@@ -7,8 +7,6 @@ const { ccclass, property } = _decorator;
 @ccclass
 export default class ToastBase<Options = any> extends Component {
 
-    @property({ type: Node, tooltip: '弹窗主体' })
-    public main: Node | undefined
 
     /** 消息持续时间 */
     @property({displayName: "持续时间"})
@@ -16,6 +14,9 @@ export default class ToastBase<Options = any> extends Component {
 
     /** 展示/隐藏动画的时长 */
     public animDuration: number = 0.3;
+
+    /** toast移动动画时长 */
+    public animMoveDuration: number = 0.5
 
     /** toast选项 */
     protected options: Options | undefined;
@@ -28,6 +29,12 @@ export default class ToastBase<Options = any> extends Component {
         this._locationIndex = value;
     }
     private _locationIndex = 0
+
+    /** Toast之间间隔 */
+    private locationInterval = 50
+
+    /** toast高度 */
+    private toastHeight = 60
 
     /** toastID */
     get toastID(): string {
@@ -50,12 +57,20 @@ export default class ToastBase<Options = any> extends Component {
             // 储存选项
             this.options = options;
             // 初始化节点
-            const main = this.main!;
+            const main = this.node;
             this.node.active = true;
             main.active = true;
-            // todo 修改动画展示
-            main.setScale(new Vec3(0.5, 0.5, 1))
+            // 动画展示
+            const mainTrans = main!.getComponent(UITransform)!
+            mainTrans.setAnchorPoint(new Vec2(0.5, 0.5))
+            main.setScale(new Vec3(1, 0.1, 1))
             main.getComponent(UIOpacity)!.opacity = 0
+            // 根据locationIndex设置高度
+            const interval = Math.floor(this.locationInterval + this.toastHeight / 2) * (this.locationIndex + 1)
+            main.addComponent(Widget)
+            main.getComponent(Widget)!.isAlignHorizontalCenter = true
+            const mainPosition = main.getPosition()
+            main.setPosition(mainPosition.x, interval, 0)
             // 初始化
             this.init(this.options!);
             // 更新样式
@@ -64,7 +79,7 @@ export default class ToastBase<Options = any> extends Component {
             let scaleTween = tween(main)
                 .to(duration, { scale: new Vec3(1,1,1)}, { easing: 'backOut' })
             let opacityTween = tween(main.getComponent(UIOpacity))
-                .to(duration, { opacity: 255 })
+                .to(duration, { opacity: 225 })
                 .call(() => {
                     // 弹窗已完全展示
                     this.onShow && this.onShow();
@@ -83,10 +98,10 @@ export default class ToastBase<Options = any> extends Component {
     public hide(duration: number = this.animDuration) {
         return new Promise<void>(res => {
             const node = this.node;
-            // todo 修改播放Toast主体动画
-            let scaleTween = tween(this.main)
-                .to(duration, { scale: new Vec3(0.5,0.5,1)}, { easing: 'backIn' })
-            let opacityTween = tween(this.main!.getComponent(UIOpacity))
+            // 播放Toast主体动画
+            let scaleTween = tween(this.node)
+                .to(duration, { scale: new Vec3(1,0.1,1)}, { easing: 'backIn' })
+            let opacityTween = tween(this.node.getComponent(UIOpacity))
                 .to(duration, { opacity: 0})
                 .call(() => {
                     // 关闭节点
@@ -103,9 +118,22 @@ export default class ToastBase<Options = any> extends Component {
         });
     }
 
-    public move(latestIndex: Number){
-        // todo 移动
-
+    /**
+     * 向上移动
+     * @param latestIndex 最新位置
+     */
+    public move(latestIndex: number){
+        // 移动
+        if (latestIndex == this.locationIndex){ return }
+        return new Promise<void>(res => {
+            // 根据locationIndex设置高度
+            this.locationIndex = latestIndex
+            const interval = Math.floor(this.locationInterval + this.toastHeight / 2) * (this.locationIndex + 1)
+            const nodePosition = this.node.position
+            tween(this.node.position)
+                .to(this.animMoveDuration, new Vec3(nodePosition.x, interval, 0))
+                .start()
+        })
     }
 
 

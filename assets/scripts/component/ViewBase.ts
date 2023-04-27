0bf3385a0307aa846bd495b7d93e15dcf03bb45f
shiqixin.set <shiqixin.set@bytedance.com>
2023-04-27 17:54:04 +0800

    [update] 页面默认切换逻辑

--- assets/scripts/component/ViewBase.ts
+++ assets/scripts/component/ViewBase.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, Label} from 'cc';
+import { _decorator, Component,Vec3, Node, Label} from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass("ViewBase")
@@ -8,12 +8,21 @@ export class ViewBase extends Component{
      * 界面展示
      * @protected
      */
-    public showVx() { }
+    public showVx() {
+        return new Promise(res => {
+            this.node.setPosition(new Vec3(0,0,0))
+
+        })
+    }
 
     /**
      * 界面隐藏
      * @protected
      */
-    public hideVx() { }
+    public hideVx() {
+        return new Promise(res => {
+            this.node.setPosition(new Vec3(0,1300,0))
+        })
+    }
 
 }

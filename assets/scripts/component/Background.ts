0dbc3bc783a93469a76018092e8c2243583d3e7e
ruokbb <420484153@qq.com>
2023-05-07 22:04:24 +0800

    [update] 背景移动

--- assets/scripts/component/Background.ts
+++ assets/scripts/component/Background.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component,UITransform, tween, Tween, Node, Vec3 } from 'cc';
+import { _decorator, Component,UITransform, tween, Tween, Node, Vec3, find} from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Background')
@@ -7,6 +7,16 @@ export class Background extends Component {
     @property({type: Number, displayName: "滚动时间", range: [0, 2, 0.1]})
     private scrollTime: number = 0.5
 
+    private startX: number = 0
+    private viewWidth: number = 0
+
+    protected onLoad() {
+        // 设置位置
+        const canvas = find("Canvas")
+        this.startX = canvas!.position.x * -1
+        this.viewWidth = canvas!.getComponent(UITransform)!.width
+        this.node.position = new Vec3(this.startX, 0, 0)
+    }
 
 
     /**
@@ -18,8 +28,7 @@ export class Background extends Component {
         Tween.stopAllByTarget(this.node)
         // 移动背景
         // @ts-ignore
-        const ViewWidth = Math.floor(this.node.getComponent(UITransform)!.width / 8)
-        const targetX = Math.floor(ViewWidth / 2) + ViewWidth * viewIndex
+        const targetX = this.startX + this.viewWidth * viewIndex * -1
         tween(this.node).to(
            this.scrollTime, {position: new Vec3(targetX, this.node.position.y, 0)}
         ).start()
